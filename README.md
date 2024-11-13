483c70485de6398933efe211d94d6a5a2996eaab
Blesware <juanfelipezoghbirobles20@gmail.com>
2024-11-12 23:55:03 -0500

    Add project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Trivia Quest
+Pequeño juego hecho en Godot
+
+Juego 2D RPG basado en trivias, que sigue una historia lineal
---
+++ scripts/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+#Metodo del boton PlayButton
+func _on_play_button_pressed() -> void:
+	pass
+
+#Metodo del boton ExitButton
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
